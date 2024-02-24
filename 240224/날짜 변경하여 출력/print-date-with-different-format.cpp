7990d471aa452bedc09aec8c173bf3f6f9bc021a
SuinLee21 <121387277+SuinLee21@users.noreply.github.com>
2024-02-24 16:46:03 +0900

    Title: 날짜 변경하여 출력, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240224/날짜 변경하여 출력/print-date-with-different-format.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int year, month, day;
+    char x;
+
+    cin >> year >> x >> month >> day;
+
+    cout << month << "-" << day << "-" << year;
+    return 0;
+}
