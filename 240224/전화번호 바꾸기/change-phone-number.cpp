759e9333935e0c76c6c1598fab3e0200ed504618
SuinLee21 <121387277+SuinLee21@users.noreply.github.com>
2024-02-24 16:48:45 +0900

    Title: 전화번호 바꾸기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240224/전화번호 바꾸기/change-phone-number.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int left, middle, right, temp;
+    char x;
+
+    cin >> left >> x >> middle >> x >> right;
+
+    temp = middle;
+    middle = right;
+    right = temp;
+
+    cout << left << x << middle << x << right;
+    return 0;
+}
