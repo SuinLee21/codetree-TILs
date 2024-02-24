b70eb2f86019b21f36a6119acb416f1d89c0225e
SuinLee21 <121387277+SuinLee21@users.noreply.github.com>
2024-02-24 16:37:36 +0900

    Title: 날짜 변경하여 출력 2, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240224/날짜 변경하여 출력 2/print-date-with-different-format-2.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int month, day, year;
+    char x;
+
+    cin >> month >> x >> day >> x >> year;
+
+    cout << year << "." << month << "." << day;
+    return 0;
+}
