ba9be82a5b5ea5b6a1ea3f7bef5508c0bc66a514
SuinLee21 <121387277+SuinLee21@users.noreply.github.com>
2024-02-26 20:02:14 +0900

    Title: 일 수 구하기, Time: 10ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240226/일 수 구하기/number-of-days-in-month.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int n;
+    cin >> n;
+
+    if (n == 2) {
+        cout << 28;
+    } else if ((n < 8 && n % 2 == 0) || (n >= 8 && n % 2 == 1)) {
+        cout << 30;
+    } else {
+        cout << 31;
+    }
+    return 0;
+}
