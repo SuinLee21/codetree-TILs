a9d7a26f2e65c2564bda90f3bf8fda3cc9e75314
SuinLee21 <121387277+SuinLee21@users.noreply.github.com>
2024-03-09 01:23:07 +0900

    Title: 중복되지 않는 숫자 중 최대, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240309/중복되지 않는 숫자 중 최대/max-of-unique-number.cpp
+++ 240309/중복되지 않는 숫자 중 최대/max-of-unique-number.cpp
@@ -10,20 +10,49 @@ int main() {
     }
 
     int max = arr[0];
+    int min = arr[0];
     bool is_duplicate = false;
 
     for (int i = 1; i < n; i++) {
-        if (arr[i] == max) {
+        if (max == arr[i]) {
             is_duplicate = true;
-        }
-        else if (arr[i] > max) {
-            max = arr[i];
+        } else if (arr[i] > max) {
             is_duplicate = false;
+            max = arr[i];
+        }
+
+        if (min > arr[i]) {
+            min = arr[i];
         }
     }
 
     if (is_duplicate) {
-        cout << -1;
+        while(is_duplicate || min != max){
+            int nextMax;
+            bool is_initialized = false;
+
+            for (int i = 0; i < n; i++) {
+                if (arr[i] >= max) {
+                    is_duplicate = true;
+                    continue;
+                }
+                if (!is_initialized) {
+                    nextMax = arr[i];
+                    is_initialized = true;
+                    is_duplicate = false;
+                    continue;
+                }
+                if (arr[i] > nextMax) {
+                    nextMax = arr[i];
+                }
+            }
+            max = nextMax;
+        }
+        if (min == max) {
+            cout << -1;
+        } else {
+            cout << max;
+        }
     } else {
         cout << max;
     }
