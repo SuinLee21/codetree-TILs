e36659be554223b45d4ef410ffa68ec49642fcad
SuinLee21 <121387277+SuinLee21@users.noreply.github.com>
2024-03-10 03:12:33 +0900

    Title: 사는 지역, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240310/사는 지역/where-live.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Data{
+    public:
+        string name, addr, city;
+
+        Data(string name = "", string addr = "", string city = "") {
+            this->name = name;
+            this->addr = addr;
+            this->city = city;
+        }
+};
+
+int main() {
+    int n;
+    Data peoples[10];
+    string name, addr, city;
+    cin >> n;
+
+    for (int i = 0; i < n; i++) {
+        cin >> name >> addr >> city;
+        peoples[i] = Data(name, addr, city);
+    }
+
+    int maxIdx = 0;
+
+    for (int i = 1; i < n; i++) {
+        if (peoples[maxIdx].name[0] < peoples[i].name[0]) {
+            maxIdx = i;
+        }
+    }
+
+    cout << "name " << peoples[maxIdx].name << endl;
+    cout << "addr " << peoples[maxIdx].addr << endl;
+    cout << "city " << peoples[maxIdx].city << endl;
+    return 0;
+}
