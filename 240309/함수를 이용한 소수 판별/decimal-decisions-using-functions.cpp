2934f7cda88d3ffb6c9d66e891beeab741eb1363
SuinLee21 <121387277+SuinLee21@users.noreply.github.com>
2024-03-09 16:55:47 +0900

    Title: 함수를 이용한 소수 판별, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240309/함수를 이용한 소수 판별/decimal-decisions-using-functions.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int SumPrime(int n) {
+    for (int i = 2; i < n; i++) {
+        if (n % i == 0) {
+            return 0;
+        }
+    }
+    
+    return n;
+}
+
+int main() {
+    int a, b, sum = 0;
+    cin >> a >> b;
+
+    for (int i = a; i <= b; i++) {
+        sum += SumPrime(i);
+    }
+    cout << sum;
+    return 0;
+}
