84fce774c99db98ee4fead9c3e83fca902e9e8e4
SuinLee21 <121387277+SuinLee21@users.noreply.github.com>
2024-03-09 16:39:28 +0900

    Title: 함수를 이용한 369 게임, Time: 9ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240309/함수를 이용한 369 게임/369-games-using-functions.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+bool ISInThree(int n) {
+    int temp = n % 10;
+    bool isThree = false;
+    while(n != 0) {
+        n /= 10;
+
+        if (temp % 3 == 0) {
+            isThree = true;
+            break;
+        }
+        temp = n % 10;
+    }
+    return isThree;
+}
+
+bool IsThree(int n) {
+    return (n % 3 == 0) || ISInThree(n);
+}
+
+int main() {
+    int a, b, count = 0;
+    cin >> a >> b;
+
+    for (int i = a; i <= b; i++) {
+        if (IsThree(i)) {
+            count++;
+        }
+    }
+    cout << count;
+    return 0;
+}
