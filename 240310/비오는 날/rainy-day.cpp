946e62ef9d2d9af7314cd8ab1b22aea8883a832e
SuinLee21 <121387277+SuinLee21@users.noreply.github.com>
2024-03-10 03:58:40 +0900

    Title: 비오는 날, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240310/비오는 날/rainy-day.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+#include <iostream>
+using namespace std;
+
+class Forcast{
+    public:
+        string date, week, weather;
+
+        Forcast(string date, string week, string weather) {
+            this->date = date;
+            this->week = week;
+            this->weather = weather;
+        }
+        Forcast() {}
+};
+
+int main() {
+    int n, idx;
+    int minYear = 2100, minMonth = 12, minDay = 31;
+    string date, week, weather;
+    Forcast forcasts[100];
+
+    cin >> n;
+
+    for (int i = 0; i < n; i++) {
+        cin >> date >> week >> weather;
+
+        forcasts[i] = Forcast(date, week, weather);
+    }
+
+    for (int i = 0; i < n; i++) {
+        int subYear = stoi(forcasts[i].date.substr(0, 4));
+        int subMonth = stoi(forcasts[i].date.substr(5, 2));
+        int subDay = stoi(forcasts[i].date.substr(8, 2));
+
+        if (forcasts[i].weather != "Rain") {
+            continue;
+        }
+        if (subYear < minYear) {
+            minYear = subYear;
+            idx = i;
+            continue;
+        }
+        if (subYear == minYear && subMonth < minMonth) {
+            minMonth = subMonth;
+            idx = i;
+            continue;
+        }
+        if (subMonth == minMonth && subDay < minDay) {
+            minDay = subDay;
+            idx = i;
+        }
+    }
+
+    cout << forcasts[idx].date << " " << forcasts[idx].week << " " << forcasts[idx].weather;
+
+    return 0;
+}
