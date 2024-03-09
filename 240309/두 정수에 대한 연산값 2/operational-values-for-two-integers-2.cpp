9155fcf2be4d92fcaef4f26554c007269d2ac8a6
SuinLee21 <121387277+SuinLee21@users.noreply.github.com>
2024-03-09 21:52:07 +0900

    Title: 두 정수에 대한 연산값 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240309/두 정수에 대한 연산값 2/operational-values-for-two-integers-2.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+void ChangeNum(int &a, int &b) {
+    if (a > b) {
+        b += 10;
+        a *= 2;
+    } else {
+        a += 10;
+        b *= 2;
+    }
+}
+
+int main() {
+    int a, b;
+    cin >> a >> b;
+
+    ChangeNum(a, b);
+
+    cout << a << " " << b;
+    return 0;
+}
